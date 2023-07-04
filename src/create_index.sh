adc90c9e3fbc38f57952cf11e6e2f287c5ef58bf
Noura Ous <noura.ousfia@gmail.com>
2023-07-04 11:12:08 +0200

    elastic serach

---
+++ src/create_index.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+curl -X PUT 'http://127.17.0.1:9200/notes' -H 'Content-Type: application/json' -d '
+{
+  "mappings": {
+    "properties": {
+      "patient_lastname": {
+        "type": "keyword"
+      },
+      "patient_firstname": {
+        "type": "keyword"
+      },
+      "text": {
+        "type": "text",
+        "analyzer": "standard"
+      },
+      "date": {
+        "type": "date"
+      },
+      "patient_left": {
+        "type": "boolean"
+      },
+      "emotion": {
+        "type": "keyword"
+      },
+      "confidence": {
+        "type": "float"
+      }
+    } 
+  }
+}
+'
+
